7396384f3c4a3420c4f230ceba4e0f3272f1e692
Ajith <ajiths0301@gmail.com>
2024-05-26 10:38:17 +0530

    latest update

---
+++ src/test/java/rahulsheddyacademy/sausedemo/uploaddowload.java
@@ -0,0 +1,25 @@
+package rahulsheddyacademy.sausedemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class uploaddowload {
+	static WebDriver driver=new ChromeDriver();
+	public static void main( String[] args ) throws InterruptedException
+    {
+	
+	//WebDriver driver=new ChromeDriver();
+	driver.get("https://www.rahulshettyacademy.com/upload-download-test/");
+	//cred rav=new cred();
+	new cred().
+	
+    }
+	public void cred()
+	{
+    // WebElement name=driver.findElement(By.id("user-name"));
+   //  WebElement password=driver.findElement(By.id("password"));
+     System.out.println("this is test");
+	}
+}
